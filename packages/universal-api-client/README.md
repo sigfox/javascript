76a88fc29779137df93466a6ee54d10930d5b0c4
Charlie Henin <charlie.henin@sigfox.com>
2019-04-10 13:25:14 +0000

    test(universal-api-client): #21 write README & tests

--- packages/universal-api-client/README.md
+++ packages/universal-api-client/README.md
@@ -1,3 +1,53 @@
 # universal-api-client
 
-## Docs (todo)
+Simple promisified http client to send requests
+
+## Features
+
+Send http request:
+
+- POST
+- GET
+- PUT
+- PATCH
+- DELETE
+- HEAD
+- OPTIONS
+
+## Install
+
+```bash
+npm install @sigfox/universal-api-client
+```
+
+## Usage
+
+```javascript
+const ApiClient = require('@sigfox/universal-api-client');
+
+const apiClient = new ApiClient({ baseURL: '/api' });
+
+const getData = client => client.get('/data');
+
+const postData = (client, data) => client.post('/data', data);
+
+getData(client).then(response => {
+  // use response here
+});
+
+postData(client, { lifePurpose: 42 }).then(response => {
+  // use response here
+});
+
+// and so on...
+```
+
+## Test
+
+```bash
+npm test
+```
+
+## Licence
+
+This project is licensed under the MIT License - see the [LICENSE](https://gitlab.partners.sigfox.com/sigfox/flive-app/blob/master/LICENSE) file for details.
--- packages/universal-api-client/rollup.config.js
+++ packages/universal-api-client/rollup.config.js
@@ -1,8 +1,5 @@
 import babel from 'rollup-plugin-babel';
-import commonJs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import sourceMaps from 'rollup-plugin-sourcemaps';
+
 import pkg from './package.json';
 
 const externalDependencies = [];
@@ -10,27 +7,18 @@ if (pkg.peerDependencies) externalDependencies.push(...Object.keys(pkg.peerDepen
 if (pkg.devDependencies) externalDependencies.push(...Object.keys(pkg.devDependencies));
 if (pkg.dependencies) externalDependencies.push(...Object.keys(pkg.dependencies));
 
-const rollupConfig = [
-  {
-    input: 'src/index.js',
-    external: externalDependencies,
-    output: [
-      { exports: 'named', format: 'cjs', sourcemap: true, file: 'lib/index.cjs.js' },
-      { format: 'esm', sourcemap: true, file: 'lib/index.esm.js' }
-    ],
-    plugins: (options = {}) => [
-      sourceMaps(),
-      json(),
-      nodeResolve(options.resolveConfig || {}),
-      babel({
-        exclude: 'node_modules/**',
-        externalHelpers: true
-      }),
-      commonJs({
-        ignoreGlobal: true
-      })
-    ]
-  }
-];
+const rollupConfig = {
+  input: 'src/index.js',
+  external: externalDependencies,
+  output: [
+    { exports: 'named', format: 'cjs', sourcemap: true, file: 'lib/index.cjs.js' },
+    { format: 'esm', sourcemap: true, file: 'lib/index.esm.js' }
+  ],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
+};
 
 export default rollupConfig;
