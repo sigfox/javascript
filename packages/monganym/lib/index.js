b956a6d62106e710f7b749c63203295ded7a1d3a
Rémy Loubradou <remy.loubradou@sigfox.com>
2019-12-05 15:58:01 +0100

    test: fix test cases and add test case

--- packages/monganym/lib/index.js
+++ packages/monganym/lib/index.js
@@ -1,3 +1,4 @@
+const { URL } = require('url');
 const { MongoClient } = require('mongodb');
 const Chance = require('chance');
 
@@ -7,11 +8,15 @@ const anonymizeCollection = require('./anonymizeCollection');
 module.exports = async function mongoanym(url, config) {
   const validation = validateConfig(config);
   if (!validation.valid) throw validation.errors[0];
-  const client = new MongoClient(url, { useUnifiedTopology: true });
+  const mongoURL = new URL(url);
+  const dbName = mongoURL.pathname.split('/')[1];
+  if (!dbName) {
+    throw new Error('Database needs to specify in the mongodb URL.');
+  }
+  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
   await client.connect();
-  if (!client.s.options.dbName) throw new Error('Database need to specify in the mongodb URL.');
-  const chance = new Chance(client.s.options.dbName);
-  const db = client.db(client.s.options.dbName);
+  const chance = new Chance(dbName);
+  const db = client.db(dbName);
   await config.collections.reduce(async (previousPromise, collectionConfig) => {
     await previousPromise;
     return anonymizeCollection(db, collectionConfig, chance);
--- packages/monganym/test/index.test.js
+++ packages/monganym/test/index.test.js
@@ -1,87 +1,69 @@
+const { URL } = require('url');
+const { describe, it, before, after } = require('mocha');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
-const {describe, it} = require('mocha');
+const Chance = require('chance');
+const mongoose = require('mongoose');
 const monganym = require('./../lib');
 const config = require('./config');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const Chance = require('chance');
+const Fixtures = require('./fixtures');
 
-const DATABASE_TEST = 'test_database';
-const MONGO_TEST_URL = process.env.MONGO_TEST_URL || `mongodb://localhost:27017/${DATABASE_TEST}`;
+chai.use(chaiAsPromised);
 
-// Create a new schema
-const anonymizeSchema = new Schema({
-  name: {type: String, required: true},
-  email: {type: String, required: true},
-  address: {
-    name: {type: String, required: true},
-    phone: {type: String, required: true},
-    country: {type: String, required: true}
-  }
-});
+const MONGO_TEST_URL = process.env.MONGO_TEST_URL || `mongodb://localhost:27017/test_db`;
 
-//Create a new collection called 'User'
-const User = mongoose.model('users', anonymizeSchema);
-const chance = new Chance('test_database');
-
-//create a new user
-const createUser = () => ({
-  name: chance.string(),
-  email: chance.string(),
-  address: {
-    name: chance.string(),
-    phone: chance.phone(),
-    country: chance.country()
-  }
-});
-
-describe('Test anonymize script', () => {
-
-  before(function (done) {
-    chai.use(chaiAsPromised);
-    this.chance = new Chance(DATABASE_TEST);
-    mongoose.connect(MONGO_TEST_URL, {useNewUrlParser: true});
+describe('monganym(url, config)', () => {
+  before((done) => {
+    mongoose.connect(MONGO_TEST_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error'));
-    db.once('open', function () {
+    db.once('error', (err) => {
+      throw err;
+    });
+    db.once('open', () => {
       done();
     });
   });
 
-  after(function (done) {
-    mongoose.connection.db.dropDatabase(function () {
+  after((done) => {
+    mongoose.connection.db.dropDatabase(() => {
       mongoose.connection.close(done);
     });
   });
 
   describe('with invalid config file', () => {
-    it('should throw an error', async () => {
-      chai.expect(monganym(MONGO_TEST_URL, {"collections": []})).to.be.rejectedWith("does not meet minimum length of 1")
+    it('should throw an error', () =>
+      chai
+        .expect(monganym(MONGO_TEST_URL, { collections: [] }))
+        .to.be.rejectedWith('does not meet minimum length of 1'));
+  });
+
+  describe('with missing database in mongodb url', () => {
+    it('should throw an error', () => {
+      const missingDatabaseURL = new URL(MONGO_TEST_URL);
+      missingDatabaseURL.pathname = '';
+      return chai
+        .expect(monganym(missingDatabaseURL.toString(), config))
+        .to.be.rejectedWith('Database needs to specify in the mongodb URL.');
     });
   });
 
   describe('with valid config file', () => {
-    it('should anonymize', async () => {
-      let nb = 0;
-      let users = [];
-      while (nb < 4) {
-        users.push(await User(createUser()).save());
-        nb++;
-      }
+    it('should anonymize data', async () => {
+      const chance = new Chance('monganym');
+      const fixtures = new Fixtures(chance);
+      const users = await fixtures.createUsers(20);
       await monganym(MONGO_TEST_URL, config);
-      await users.forEach(user => {
-        User.findOne({_id: user._id}, async (err, elt) => {
-          if (err) {
-            throw err
-          }
-          chai.expect(elt.name).to.not.equal(user.name);
-          chai.expect(elt.email).to.not.equal(user.email);
-          chai.expect(elt.address.name).to.not.equal(user.address.name);
-          chai.expect(elt.address.phone).to.not.equal(user.address.phone);
-          chai.expect(elt.address.country).to.equal(user.address.country);
-        });
-      });
+      const User = fixtures.UserModel;
+      await Promise.all(
+        users.map(async (user) => {
+          const anonymizedUser = await User.findOne({ _id: user._id });
+          chai.expect(anonymizedUser.name).to.not.equal(user.name);
+          chai.expect(anonymizedUser.email).to.not.equal(user.email);
+          chai.expect(anonymizedUser.address.name).to.not.equal(user.address.name);
+          chai.expect(anonymizedUser.address.phone).to.not.equal(user.address.phone);
+          chai.expect(anonymizedUser.address.country).to.equal(user.address.country);
+        })
+      );
     });
   });
 });
