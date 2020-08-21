57a7126dd003357259e73a0ff2b7279f3bec0cbd
Remy Loubradou <remy.loubradou@sigfox.com>
2020-08-21 17:16:43 +0200

    feat(koa-prometheus-http-metrics): add http_requests_total metric to collect total number of http requests
    the number of requests is collected with the dimensions method, handler, and status.
    
    BREAKING CHANGE: remove the possibility to change `labelNames` of the histogram metric `http_request_duration_ms`, before changing these values would actually break the correct behavior and with a new metric `http_requests_total`. Also, remove the possibility to change `name` and `help` of the histogram metric `http_request_duration_ms` since now two metrics are collected with different `name` and `help` values.

--- packages/koa-prometheus-http-metrics/index.js
+++ packages/koa-prometheus-http-metrics/index.js
@@ -3,31 +3,37 @@ const Prometheus = require('prom-client');
 let httpRequestDurationMs;
 
 module.exports = (options = {}) => {
-  const {
-    name = 'http_request_duration_ms',
-    help = 'Duration of HTTP requests in ms',
-    labelNames = ['route'],
-    buckets = [50, 100, 300, 500],
-    register = Prometheus.register
-  } = options;
+  const { buckets = [50, 100, 300, 500], register = Prometheus.register } = options;
   const { filter, ...restOptions } = options;
   httpRequestDurationMs = new Prometheus.Histogram({
-    name,
-    help,
-    labelNames,
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route'],
     buckets,
+    registers: [register],
+    ...restOptions
+  });
+  const httpRequestsTotal = new Prometheus.Counter({
+    name: 'http_requests_total',
+    help: 'The total number of HTTP requests made',
+    labelNames: ['method', 'handler', 'statuscode'],
     registers: [register],
     ...restOptions
   });
   return async (ctx, next) => {
     ctx.state.requestStartTime = Date.now();
     await next();
-    const requestduration = Date.now() - ctx.state.requestStartTime;
+    const requestDuration = Date.now() - ctx.state.requestStartTime;
     // prefers routerPath over path, routerPath is less specific
     // and is available in @koajs/router@9.1.0
     const path = ctx.routerPath || ctx.path;
     const shouldObserve = filter ? filter(path) : true;
-    if (shouldObserve) httpRequestDurationMs.labels(path).observe(requestduration);
+    const { method } = ctx.request;
+    const { status } = ctx.response;
+    if (shouldObserve) {
+      httpRequestDurationMs.labels(method, path).observe(requestDuration);
+      httpRequestsTotal.labels(method, path, status).inc();
+    }
   };
 };
 module.exports.httpRequestDurationMs = httpRequestDurationMs;
